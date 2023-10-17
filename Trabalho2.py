49cf998ade68cff06b3ded0a42bbb619755deae0
ThayssonBueno <148142896+ThayssonBueno@users.noreply.github.com>
2023-10-16 21:44:38 -0300

    Update Trabalho2.py

--- Trabalho2.py
+++ Trabalho2.py
@@ -1,1 +1,6 @@
-print("ola")
+n1 = float(input("Aluno Ana")) 
+n2 = float(input("Aluno Beatriz:")) 
+n3 = float(input("Aluno Pedro:")) 
+n4 = float(input("Aluno Ze:")) 
+media = (n1+n2+n3+n4)/2
+print("A média entre {:.1f} e {:.1f} é igual a {:.1f}".format(n1, n2, n3, n4, media))
