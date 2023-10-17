e334ee3c6fc7917a2c160d734aa2e65dbc2fb269
ThayssonBueno <148142896+ThayssonBueno@users.noreply.github.com>
2023-10-17 15:40:47 -0300

    Update Trabalho2.py

--- Trabalho2.py
+++ Trabalho2.py
@@ -1,6 +1,6 @@
-n1 = float(input("Aluno Ana 01")) 
-n2 = float(input("Aluno Beatriz 02:")) 
-n3 = float(input("Aluno Pedro 03:")) 
-n4 = float(input("Aluno Ze 04:")) 
+n1 = float(input("Nota Pedro 5.5")) 
+n2 = float(input("Nota Pedro 7:")) 
+n3 = float(input("Nota Pedro 6:")) 
+n4 = float(input("Nota Pedro 9:")) 
 media = (n1+n2+n3+n4)/2
 print("A média é {:.1f}".format(media))
