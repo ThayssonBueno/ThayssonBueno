a67d8126a91f31a3c3c63ef941ff7b3226954575
ThayssonBueno <148142896+ThayssonBueno@users.noreply.github.com>
2023-10-18 20:48:24 -0300

    Update Trabalho2.py

--- Trabalho2.py
+++ Trabalho2.py
@@ -2,5 +2,5 @@ n1 = float(input("Nota Pedro 5.5"))
 n2 = float(input("Nota Pedro 7:")) 
 n3 = float(input("Nota Pedro 6:")) 
 n4 = float(input("Nota Pedro 9:")) 
-media = (n1+n2+n3+n4)/2
+media = (n1+n2+n3+n4)/4
 print("A média é {:.1f}".format(media))
